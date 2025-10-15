5cbf789e9f67a106504fea70f1eaf9fa8a0544c5
Lin Tran <lin.tran@asnet.com.vn>
2025-10-15 18:32:19 +0700

    Config database

---
+++ src/database/db.ts
@@ -0,0 +1,11 @@
+// Libs
+import { drizzle } from 'drizzle-orm/node-postgres';
+import { Pool } from 'pg';
+
+import { usersSchema } from '../schema/userSchema';
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+export const db = drizzle(pool, { schema: { users: usersSchema } });
