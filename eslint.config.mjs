d16ea576d9738bbd887c5f36e10cdb9eef20d8b7
Lin Tran <lin.tran@asnet.com.vn>
2025-10-16 18:16:15 +0700

    install transactional and config path

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -52,4 +52,11 @@ export default tseslint.config(
       "@typescript-eslint/no-unsafe-assignment": "off",
     },
   },
+  {
+    files: ["src/database/database.module.ts"],
+    rules: {
+      "@typescript-eslint/no-unsafe-call": "off",
+      "@typescript-eslint/no-unsafe-assignment": "off",
+    },
+  }
 );
