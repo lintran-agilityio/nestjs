5b9cd0a1fb76e13d0ab51380a078d5e9ff28501c
Lin Tran <lin.tran@asnet.com.vn>
2025-10-17 18:32:21 +0700

    using and config typeorm

---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,16 @@
+// libs
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+import { User } from '@app/user/entities/user.entity';
+
+export const typeOrmConfig: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: process.env.DB_HOST || 'localhost',
+  port: parseInt(process.env.DB_PORT || '5432', 10),
+  username: process.env.DB_USER || 'postgres',
+  password: process.env.DB_PASS || 'postgres',
+  database: process.env.DB_NAME || 'fake_social',
+  entities: [User],
+  synchronize: true,
+  logging: true,
+};
