bf03ba77d069e4802db0df371597ce549144b63e
Lin Tran <lin.tran@asnet.com.vn>
2025-10-17 18:30:41 +0700

    update dizzle orm become type orm

--- src/database/schema/common.ts
+++
@@ -1,8 +0,0 @@
-import { timestamp } from 'drizzle-orm/pg-core';
-
-export const baseSchema = {
-  createdAt: timestamp('created_at').defaultNow().notNull(),
-  updatedAt: timestamp('updated_at')
-    .defaultNow()
-    .$onUpdateFn(() => new Date()),
-};
