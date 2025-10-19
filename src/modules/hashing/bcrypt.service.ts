ab9742b260e2fd5b1fbc59f306517ccae05c3007
Lin Tran <lin.tran@asnet.com.vn>
2025-10-19 17:12:11 +0700

    Split bcrypt and hashing password services

---
+++ src/modules/hashing/bcrypt.service.ts
@@ -0,0 +1,17 @@
+// libs
+import { Injectable } from '@nestjs/common';
+import { compare, genSalt, hash } from 'bcryptjs';
+
+import { HashingService } from './hashing.service';
+
+@Injectable()
+export class BcryptService implements HashingService {
+  async hash(data: string | Buffer): Promise<string> {
+    const salt = await genSalt(10);
+    return hash(data.toString(), salt);
+  }
+
+  async compare(data: string | Buffer, hash: string): Promise<boolean> {
+    return compare(data.toString(), hash);
+  }
+}
