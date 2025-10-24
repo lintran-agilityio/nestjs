6fa45df37c14bdac4f1068beb40e28672ff0979c
Lin Tran <lin.tran@asnet.com.vn>
2025-10-24 11:29:57 +0700

    Add exclude password when response data

--- src/modules/user/entities/user.entity.ts
+++ src/modules/user/entities/user.entity.ts
@@ -3,12 +3,14 @@ import { Entity, Column, OneToMany } from 'typeorm';
 import { UserRole, UserStatus } from '../../../shared/types';
 import { BaseEntity } from '../../../shared/entities';
 import { Post } from '../../post/entities';
+import { Exclude } from 'class-transformer';
 
 @Entity('users')
 export class User extends BaseEntity {
   @Column({ name: 'email', type: 'varchar', length: 255, unique: true })
   email: string;
 
+  @Exclude()
   @Column({ name: 'password', type: 'varchar', length: 255 })
   password: string;
 
